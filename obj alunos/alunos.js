cb952a03f43b8b46b9bfd4824c7c50d6392610cd
Luiz Ricardo Maciel <arisdeck01@gmail.com>
2024-06-29 17:54:02 -0300

    alunos caluladora

---
+++ obj alunos/alunos.js
@@ -0,0 +1,90 @@
+let alunos = []
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+// Array para armazenar os alunos
+let alunos = [];
+
+// Função para cadastrar um novo aluno
+function cadastrarAluno(nome, idade, curso) {
+    let aluno = {
+        nome: nome,
+        idade: idade,
+        curso: curso
+    };
+    alunos.push(aluno);
+}
+
+// Função para listar todos os alunos cadastrados
+function listarAlunos() {
+    console.log("Lista de Alunos:");
+    alunos.forEach(function(aluno) {
+        console.log(`Nome: ${aluno.nome}, Idade: ${aluno.idade}, Curso: ${aluno.curso}`);
+    });
+}
+
+// Função para pesquisar alunos por curso
+function pesquisarPorCurso(curso) {
+    console.log(`Alunos do curso ${curso}:`);
+    alunos.forEach(function(aluno) {
+        if (aluno.curso === curso) {
+            console.log(`Nome: ${aluno.nome}, Idade: ${aluno.idade}`);
+        }
+    });
+}
+
+// Função para remover um aluno pelo nome
+function removerAluno(nome) {
+    alunos = alunos.filter(function(aluno) {
+        return aluno.nome !== nome;
+    });
+}
+
+// Exemplos de utilização
+
+// Cadastro de alguns alunos
+cadastrarAluno("Maria", 20, "Engenharia");
+cadastrarAluno("João", 22, "Medicina");
+cadastrarAluno("Ana", 21, "Direito");
+
+// Listagem de todos os alunos
+listarAlunos();
+
+// Pesquisa por alunos do curso de Medicina
+pesquisarPorCurso("Medicina");
+
+// Remoção do aluno "Maria"
+removerAluno("Maria");
+
+// Listagem atualizada após remoção
+listarAlunos();
+
+*/
