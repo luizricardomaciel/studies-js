86cc3dce9f32ae88c0af2f157e44f20103c13f30
Luiz Ricardo Maciel <arisdeck01@gmail.com>
2024-06-30 15:48:11 -0300

    att studies

--- obj alunos/alunos.js
+++ obj alunos/alunos.js
@@ -1,16 +1,39 @@
+
 let alunos = []
 
+function cadastrarAluno(nome, idade, curso) {
+    let aluno = {
+        nome : nome,
+        idade : idade ,
+        curso : curso
+    };
+    alunos.push(aluno);
+}
 
+cadastrarAluno("maria", 23, "dev")
+cadastrarAluno("robert", 25, "dev")
+cadastrarAluno("jao", 21, "professor")
 
+function listarAlunos () {
+    console.log ("------LISTAGEM DE ALUNOS------");
+    alunos.forEach(function(i) {
+        console.log(`aluno(@): ${i.nome}, idade: ${i.idade}, curso: ${i.curso}`);
+    });
+}
+listarAlunos ()
 
+function pesquisarPorCurso (i) {
+    console.log("-------PESQUISA DE ALUNOS-------");
+    let alunosCurso = []
+    alunos.forEach(function(aluno){
+        if (i == aluno.curso) {
+            alunosCurso += aluno.nome + aluno.idade
+            //console.log(`No curso de ${i} temos ${aluno.nome} de ${aluno.idade}`)
+        }
+    });
+}
 
-
-
-
-
-
-
-
+pesquisarPorCurso (`dev`)
 
 
 
