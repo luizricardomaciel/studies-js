6300ac3983e6a7f101a515bc45be8c29ca98622b
Luiz Ricardo Maciel <arisdeck01@gmail.com>
2024-08-06 11:39:04 -0300

    Forming a Magic Square

---
+++ Forming a magic square/script.js
@@ -0,0 +1,36 @@
+const squareMagic = {
+  1: [4, 9, 2, 3, 5, 7, 8, 1, 6],
+  2: [2, 7, 6, 9, 5, 1, 4, 3, 8],
+  3: [6, 1, 8, 7, 5, 3, 2, 9, 4],
+  4: [8, 3, 4, 1, 5, 9, 6, 7, 2],
+  5: [8, 1, 6, 3, 5, 7, 4, 9, 2],
+  6: [6, 7, 2, 1, 5, 9, 8, 3, 4],
+  7: [4, 3, 8, 9, 5, 1, 2, 7, 6],
+  8: [2, 9, 4, 7, 5, 3, 6, 1, 8],
+};
+
+const s = [
+  [5, 3, 4],
+  [1, 5, 8],
+  [6, 4, 2],
+];
+
+function formingMagicSquare(s) {
+  const array = s.flat();
+  let result = Infinity;
+  for (let square in squareMagic) {
+    let minus = null;
+    for (let i = 0; i < array.length; i++) {
+      if (squareMagic[square][i] > array[i]) {
+        minus += squareMagic[square][i] - array[i];
+      } else {
+        minus += array[i] - squareMagic[square][i];
+      }
+    }
+    if (result > minus) {
+      result = minus;
+    }
+  }
+  console.log(result);
+}
+formingMagicSquare(s);
